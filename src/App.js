c4dee8be8412ad939f5f8aab6f0d8e1ca2d53c3e
gregkillebrew <gkillebrew91@gmail.com>
2022-08-01 14:58:22 -0500

    getting there

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        arrayOfBeer:[],
+      
+      };
+    }
 
-export default App;
+    componentDidMount(){
+      fetch('https://api.punkapi.com/v2/beers')
+       .then(response => {
+        return response.json()
+       }).then(arrayOfBeer => this.setState({arrayOfBeer}))
+   }
+
+    render() {
+      return (
+          <div>
+             <h2>BEERS!</h2>
+
+             <ol>{this.state.arrayOfBeer.map((beer, index) => {
+              return (
+                <li key={index}><h3>{beer.name}</h3> <h4>{beer.tagline}</h4>
+                </li>)
+            })}
+            </ol>
+
+          </div>
+
+          
+      )
+    }
+  }
+  
+  
+
+ 
+
+
+
+  export default App;
